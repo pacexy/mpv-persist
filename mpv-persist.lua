eb16aceb1bd6cacf653f527f80f7a329ac056056
pacexy <pacexy@gmail.com>
2024-11-24 19:40:12 +0800

    refactor: rename

--- mpv-persist.lua
+++ mpv-persist.lua
@@ -11,7 +11,7 @@ options.read_options(opts)
 
 local suppress_write = false
 
-local function get_conf_path()
+local function get_dir_conf_path()
   local path = mp.get_property("path")
   -- path is nil during loading file
   if not path then return end
@@ -20,60 +20,62 @@ local function get_conf_path()
   return utils.join_path(dir_path, "mpv.conf")
 end
 
-local function parse_options_from_file(conf_path)
-  local options_table = {}
+local function parse_properties_from_file(conf_path)
+  local properties_table = {}
   local file = io.open(conf_path, "r")
   if file then
     for line in file:lines() do
       local key, value = line:match("([^=]+)=([^=]+)")
       if key and value then
-        options_table[key] = value
+        properties_table[key] = value
       end
     end
     file:close()
   end
-  return options_table
+  return properties_table
 end
 
-local function write_options_to_file(conf_path, options_table)
+local function write_properties_to_file(conf_path, properties_table)
   local file = io.open(conf_path, "w")
   if not file then
     mp.msg.error("Could not open file for writing: " .. conf_path)
     return
   end
-  for key, value in pairs(options_table) do
+  for key, value in pairs(properties_table) do
     file:write(key .. "=" .. value .. "\n")
   end
   file:close()
 end
 
--- Function to write options to a directory-specific config
-local function write_options()
+
+local function save_properties()
   if suppress_write then return end
-  local conf_path = get_conf_path()
+  local conf_path = get_dir_conf_path()
   if not conf_path then return end
   mp.msg.info("Saving options to " .. conf_path)
 
-  local options_table = parse_options_from_file(conf_path)
+  local properties_table = parse_properties_from_file(conf_path)
 
+  -- merge the properties
   for _, prop in ipairs(opts.props) do
     local value = mp.get_property(prop)
     if value then
-      options_table[prop] = value
+      properties_table[prop] = value
     end
   end
 
-  write_options_to_file(conf_path, options_table)
+  write_properties_to_file(conf_path, properties_table)
 end
 
--- Function to load options from a directory-specific config
-local function load_options()
-  local conf_path = get_conf_path()
+local function load_properties()
+  local conf_path = get_dir_conf_path()
   if not conf_path then return end
   mp.msg.info("Loading options from " .. conf_path)
 
-  local options_table = parse_options_from_file(conf_path)
-  for key, value in pairs(options_table) do
+  local properties_table = parse_properties_from_file(conf_path)
+
+  -- set the properties
+  for key, value in pairs(properties_table) do
     mp.set_property(key, value)
   end
 
@@ -81,7 +83,7 @@ local function load_options()
 end
 
 for _, prop in ipairs(opts.props) do
-  mp.observe_property(prop, "native", write_options)
+  mp.observe_property(prop, "native", save_properties)
 end
 
 mp.register_event("start-file", function()
@@ -90,7 +92,7 @@ end)
 
 mp.register_event("file-loaded", function()
   suppress_write = false
-  load_options()
+  load_properties()
 end)
 
-mp.register_event("shutdown", write_options)
+mp.register_event("shutdown", save_properties)
